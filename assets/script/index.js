b295067994f2b6f919563a12eee1287c08f2dc3e
Marcelolop <marcelolopfern@hotmail.com>
2023-11-06 12:58:27 -0600

    undoing las commit

--- assets/script/index.js
+++ assets/script/index.js
@@ -43,10 +43,6 @@ function getOperatingSystemName() {
     osName = "MacOS";
   } else if (userAgent.includes("Linux")) {
     osName = "Linux";
-  } else if (userAgent.includes("Android")) {
-    osName = "Android";
-  } else if (userAgent.includes("iOS")) {
-    osName = "iOS";
   }
   return `Current OS: ${osName}`;
 }
